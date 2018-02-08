bdfec5b030479e7bf88a2af9388e89af89468bdc
jcerda <jccer103@gmail.com>
2018-02-07 23:59:35 -0600

    added request module

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var key = require("./movie-app/src/config/tmdb")
+var request = require("request");
 
 const express = require('express')
 const app = express()
@@ -12,12 +13,22 @@ app.use(bodyParser.json())
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/search', (req, res) => {
-  app.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${key.tmdb_api_key}&language=en-US&page=1`, function(err,results) {
-    if (err) console.log(err);
-      res.send(results)
-  });
-   
-  // res.send('hello') 
+  var options = { method: 'GET',
+  url: 'https://api.themoviedb.org/3/movie/now_playing',
+  qs: 
+   { api_key: `${key.tmdb}`,
+     language: 'en-US',
+     page: '1' },
+  headers: 
+   { 'Postman-Token': '4a5abcd4-a69c-906d-676b-8ec2f4211b32',
+     'Cache-Control': 'no-cache' } };
+
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  console.log(body);
+});
+
 });
 
 
